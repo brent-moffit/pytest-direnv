283c44d9c2780942c912c7d369f014b19d381371
brent-moffit <72231013+brent-moffit@users.noreply.github.com>
2021-10-18 18:38:12 -0700

    Fix name in direnv cmd

--- pytest_direnv/direnv.py
+++ pytest_direnv/direnv.py
@@ -3,7 +3,7 @@ import subprocess
 
 def load_vars() -> dict:
     result = subprocess.check_output(
-        "direnv exec . python -m pyenv_direnv.direnv_export",
+        "direnv exec . python -m pytest_direnv.direnv_export",
         shell=True,
     )
 
